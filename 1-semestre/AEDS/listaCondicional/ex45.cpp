9bf13cdcd3914df96a50dcc0a49877c7be4ca211
KAIO LEONARDO MACIEL NEVES <10372006671@campusrp-lab.riopomba.ifsudestemg.edu.br>
2026-04-06 07:59:58 -0300

    ex 45 feito

---
+++ 1-semestre/AEDS/listaCondicional/ex45.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+float aumento(float salario, int cod){
+	switch(cod){
+		case 101:{
+			float salarioN= salario+(salario*0.10);
+			return salarioN;
+			break;
+		}
+		case 102:{
+			float salarioN= salario+(salario*0.20);
+			return salarioN;
+			break;
+		}
+		case 103:{
+			float salarioN= salario+(salario*0.30);
+			return salarioN;
+			break;
+		}
+		default:{
+			float salarioN= salario+(salario*0.40);
+			return salarioN;
+			break;
+		}		
+	}	
+}
+
+int main(){
+	float salario; int codigo;
+	
+	cout<<"Codigo |    Cargo   | Percentual"<<endl;
+	cout<<"  101  |   Gerente  |    10%    "<<endl;
+	cout<<"  102  | Engenheiro |    20%    "<<endl;
+	cout<<"  103  |   Tecnico  |    30%    "<<endl;
+	
+	cout<<"Informe o seu salario: ";
+	cin>>salario;
+	
+	cout<<"Informe o codigo da sua profissao: ";
+	cin>>codigo;
+	
+	cout<<"Seu novo salario e de: R$ "<<aumento(salario, codigo);
+}
