83b073f6e18de2d3f8334292a441c700b7f23a0b
KAIO LEONARDO MACIEL NEVES <10372006671@campusrp-lab.riopomba.ifsudestemg.edu.br>
2026-04-14 14:12:24 -0300

    ex2 feito

---
+++ 1-semestre/AEDS/listaRepeticao/ex2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void maiorMenor(){
+	int quantidade;
+	cout<<"Digite a quantidade de numeros: ";
+	cin>>quantidade;
+	int num;
+	int maior = 0;
+	
+	while(quantidade>0){
+		quantidade--;
+		cin>>num;
+		if(num>maior){
+			maior = num;
+		}
+	}
+	cout<<"Esse e o maior numero: "<<maior;
+}
+
+int main(){
+	
+	maiorMenor();
+	
+return 0;	
+}
