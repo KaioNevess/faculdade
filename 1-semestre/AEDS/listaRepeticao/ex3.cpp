212480028b876b9fd31b070155b936b49b7772ff
KAIO LEONARDO MACIEL NEVES <10372006671@campusrp-lab.riopomba.ifsudestemg.edu.br>
2026-04-14 14:37:11 -0300

    ex 3 feito

---
+++ 1-semestre/AEDS/listaRepeticao/ex3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void somaMedia(){
+	int quantidade;
+	cout<<"Digite a quantidade de numeros: ";
+	cin>>quantidade;
+	int num;
+	int soma = 0;
+	float media;
+	
+	
+	while(quantidade>0){
+		quantidade--;
+		cin>>num;
+		
+		soma = soma + num;
+		
+		media = soma/num;
+		
+		
+	}
+	
+	cout<<"soma: "<<soma<<endl;
+	cout<<"media: "<<media;
+}
+
+	
+
+
+int main(){
+	
+	somaMedia();
+	
+return 0;	
+}
