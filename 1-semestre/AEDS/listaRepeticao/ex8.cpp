768694173e03b6f81e8a5feb8fe020035d7aabbd
Kaio Neves <148585720+KaioNevess@users.noreply.github.com>
2026-05-11 07:41:51 -0300

    Atualização doo ex8.cpp

--- 1-semestre/AEDS/listaRepeticao/ex8.cpp
+++ 1-semestre/AEDS/listaRepeticao/ex8.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void notas(float nota){
-	for(int i = 0; nota>=1;){
-		cout<<"Digite sua nota: ";
-		cin>>nota;
-		
-		if(nota>10){
-			cout<<" Nota anterior invalida"<<endl;
-		}
-	}
+void notas(){
+        float nota;
+        do{
+                cout<<"Digite sua nota: ";
+                cin>>nota;
+
+                if(nota>10){
+                        cout<<" Nota anterior invalida"<<endl;
+                }
+                }while(nota>=1);
+        
 }
 
 int main(){
-	float n;
-	cout<<"Digite sua nota: ";
-	cin>>n;
-	notas(n);
+        
+        notas();
 }
